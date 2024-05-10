7adb2830d5831176994e2d4c61c8a121d976948c
Kavitha0907 <168117493+Kavitha0907@users.noreply.github.com>
2024-05-10 15:00:38 +0530

    MulTable.java

---
+++ MulTable.java
@@ -0,0 +1,12 @@
+
+
+//User function Template for Java
+class Solution{
+    static ArrayList<Integer> getTable(int N){
+        ArrayList<Integer> k=new ArrayList<Integer>();
+        for(int i=1;i<=10;i++)
+        k.add(N*i);
+        return k;
+        // code here
+    }
+}
